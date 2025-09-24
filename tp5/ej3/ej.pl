c4e7dfbf9a1ee3e9dd33dadb2031be993daf9d94
AlieniAgustin <alieniagustinweb@gmail.com>
2025-09-24 08:51:55 -0300

    add ej3 and modify ej7

---
+++ tp5/ej3/ej.pl
@@ -0,0 +1,7 @@
+arc(a,b).
+arc(a,c).
+arc(a,f).
+arc(c,b).
+arc(b,d).
+path(X,Y) :- arc(X,Y).
+path(X,Z) :- arc(X,Y), path(Y,Z).
--- tp5/ej7/ej.pl
+++ tp5/ej7/ej.pl
@@ -2,3 +2,4 @@ sub_set([],[]).
 sub_set(XS,[]).
 sub_set([X|XS],[X|YS]) :- sub_set(XS,YS).
 sub_set([X|XS],YS) :- sub_set(XS,YS).
+
